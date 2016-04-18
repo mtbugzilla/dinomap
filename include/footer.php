e7a5dd2207f9af5c3ddf8f26dc8df8e03fab4cae
Bugzilla@Twinoid <mtbugzilla@users.noreply.github.com>
2016-04-18 20:54:54 +0200

    Updated footer.

--- include/footer.php
+++ include/footer.php
@@ -1,9 +1,18 @@
 </div>
 </div>
 <div id="footer">
-© 2015 - <a href="/">Site</a> créé par <a href="http://twinoid.com/user/148">Bugzilla @ Twinoid</a>.
+<?php
+echo localized_msg([
+  "en" => '<a href="/">Site</a> created by <a href="http://twinoid.com/user/148">Bugzilla @ Twinoid</a> - © 2016.
+This unofficial fan site is independent from <a href="https://motion-twin.com/en/">Motion Twin</a>.<br />
+The source code for this site is availabe on <a href="https://github.com/mtbugzilla/dinomap">GitHub</a> under the license "GNU Affero GPL 3.0".<br />
+The images in the "images_mt" directory belong to Motion Twin - © 2008-2015.',
+  "fr" => '<a href="/">Site</a> créé par <a href="http://twinoid.com/user/148">Bugzilla @ Twinoid</a> - © 2016.
 Ceci est un site non officiel et non lié à la <a href="https://motion-twin.com/fr/">Motion Twin</a>.<br />
-Les images du répertoire "images_mt" sont la propriété de Motion Twin - © 2008-2015.
+Le code source pour ce site est disponible sur <a href="https://github.com/mtbugzilla/dinomap">GitHub</a> sous la license GNU Affero GPL 3.0.<br />
+Les images du répertoire "images_mt" sont la propriété de Motion Twin - © 2008-2015.'
+]);
+?>
 </div>
 </body>
 </html>
